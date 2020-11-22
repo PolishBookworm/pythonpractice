3992b159dde43f8062dee28e78ec4ffb2ea4dc86
Oliwka <oliwka.pszona@gmail.com>
2020-11-22 18:15:33 +0100

    Resolved issue #2

--- practicepython/6.py
+++ practicepython/6.py
@@ -1,6 +1,6 @@
 string = input('Give me a string\n')
 gnirts = string[::-1]
-if gnirts.lower == string.lower:
+if gnirts.lower() == string.lower():
     print(string + ' is a palindrome.')
 else:
     print(string + ' is not a palindrome.')
