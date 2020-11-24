82e10adf23d60eb9300769387dea72b0aed3782e
Oliwka <oliwka.pszona@gmail.com>
2020-11-24 17:54:18 +0100

    solved issue #7, added test file

---
+++ test.py
@@ -0,0 +1,15 @@
+##### Just testing some stuff to add it to my code
+
+# reverse() vs reversed()
+
+fellowship_of_the_ring = ["Frodo", "Sam", "Pippin", "Merry", "Gandalf", "Aragorn", "Boromir", "Legolas", "Gimli"]
+
+print(fellowship_of_the_ring)
+print(fellowship_of_the_ring.reverse())
+print(reversed(fellowship_of_the_ring))
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+
+print(primes)
+# print(primes.reverse())
+print(list(reversed(primes)))
