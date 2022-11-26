c591a01a124831fa6cf53c2944a2f6195e9c1521
Oliwia <oliwka.pszona@gmail.com>
2022-11-26 12:06:29 +0100

    Cleaned up some empty files

--- practicepython/13.py
+++ practicepython/13.py
@@ -1,21 +1,22 @@
-# NOT FINISHED!! (make it recursive)
+# NOT FINISHED!!
+# Write a program that asks the user how many Fibonnaci numbers to
+# generate and then generates them.
 
-# Write a program that asks the user how many Fibonnaci numbers to generate and then generates them.
+def Fibs(n):
+    '''Generates n Fibbonaci numbers'''
 
-def Fibs(amount):
-    '''Generates amount of Fibbonaci numbers'''
-
-    if amount <= 0:
+    if n <= 0:
         raise ValueError("Values greater than or equal to 0 not allowed.")
-    if amount == 1:
+    if n == 1:
         return [1]
-    if amount == 2:
+    if n == 2:
         return [1, 1]
-    
+    prev = Fibs(n - 1)
+    return prev.append(prev[-1] + prev[-2])
 
 
 print(Fibs(10))
-print(Fibs(-1))
-print(Fibs(0))
+# print(Fibs(-1))
+# print(Fibs(0))
 print(Fibs(1))
 print(Fibs(2))
---
+++ sierpinski_triangle.py
@@ -0,0 +1,5 @@
+from turtle import forward, right
+def triangle(a):
+	for n in range(3):
+		forward(a)
+		right(120)
