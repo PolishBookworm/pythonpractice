6af29191b23739cb952ff11a27f435a541b35ad0
oliwka <oliwka.pszona@gmail.com>
2023-01-05 17:23:51 +0100

    AoC files

--- The_Modern_Python3_Bootcamp/ascii_art.py
+++ The_Modern_Python3_Bootcamp/ascii_art.py
@@ -2,9 +2,9 @@ from pyfiglet import figlet_format
 from termcolor import colored
 from random import choice
 
-colors = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")
+COLORS = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")
 
-def ascii(text, color=choice(colors)):
+def ascii(text, color=choice(COLORS)):
     return colored(figlet_format(text), color)
 
 
@@ -12,14 +12,7 @@ if __name__ == "__main__":
     text = input("What message would you like to print? ")
     color = input("What color? ")
 
-    if color not in colors:
-        color = choice(colors)
+    if color not in COLORS:
+        color = choice(COLORS)
 
     print(ascii(text, color))
-
-
-# try:
-#     result = colored(figlet_format(text), color)
-# except KeyError:
-#     result = colored(figlet_format(text), color = choice(["red", "green", "yellow", "blue", "magenta", "cyan", "white"]))
-# print(result)
